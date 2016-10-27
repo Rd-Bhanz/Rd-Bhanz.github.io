cb656159e0d0e9e295edbc3f49b4dc721d0cdc7d
Rd-Bhanz <rdudayan2@gmail.com>
2016-10-27 18:22:27 +0530

    all blocks intiated

--- phiro/phirov1.js
+++ phiro/phirov1.js
@@ -8,18 +8,99 @@
         return {status: 2, msg: 'Ready'};
     };
 
-    ext.my_first_block = function() {
+    ext.disconnect_phiro = function() {
         // Code that gets executed when the block is run
     };
+
+    ext.left_red = function() {
+       //Code to turn on the led on the left to red
+    };
+
+    ext.left_green = function() {
+       //Code to turn on the led on the left to green
+    };
+
+    ext.left_blue = function() {
+       //Code to turn on the led on the left to blue
+    };
+
+    ext.right_red = function() {
+       //Code to turn on the led on the right to red
+    };
+
+    ext.right_green = function() {
+       //Code to turn on the led on the right to green
+    };
+
+    ext.right_blue = function() {
+       //Code to turn on the led on the right to blue
+    };
+
+    ext.left_motor_forward = function() {
+       //Code to turn on the motor on the left side to spin forward
+    };
+
+    ext.left_motor_backward = function() {
+       //Code to turn on the motor on the left side to spin backward
+    };
+
+    ext.right_motor_forward = function() {
+       //Code to turn on the motor on the right side to spin forward
+    };
+
+    ext.right_motor_backward = function() {
+       //Code to turn on the motor on the right side to spin backward
+    };
+
+    ext.side_right_sensor = function() {
+       //Code to read from the sensor on the right side
+    };
+
+    ext.front_right_sensor = function() {
+       //Code to read from the sensor on the front of the right side
+    };
+
+    ext.bottom_right_sensor = function() {
+       //Code to read from the sensor on the bottom of the right side
+    };
+
+    ext.side_left_sensor = function() {
+       //Code to read from the sensor on the left side
+    };
+
+    ext.front_left_sensor = function() {
+       //Code to read from the sensor on the front of the left side
+    };
+
+    ext.bottom_left_sensor = function() {
+       //Code to read from the sensor on the bottom of the left side
+    };
+
 
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
-            // Block type, block name, function name
-            [' ', 'my first block', 'my_first_block'],
+            // Block type, block name, function name, input_value
+            [' ', 'Disconnect_PHIRO', 'disconnect_phiro', 255],
+            [' ', 'Left_RED', 'left_red', 255],
+            [' ', 'Left_GREEN', 'left_green', 255],
+            [' ', 'Left_BLUE', 'left_blue', 255],
+            [' ', 'RIGHT_RED', 'right_red', 255],
+            [' ', 'RIGHT_GREEN', 'right_green', 255],
+            [' ', 'RIGHT_BLUE', 'right_blue', 255],
+            [' ', 'LeftMotorForward', 'left_motor_forward', 255],
+            [' ', 'LeftMotorBackward', 'left_motor_backward', 255],
+            [' ', 'RightMotorForward', 'right_motor_forward', 255],
+            [' ', 'RightMotorBackward', 'right_motor_backward', 255],
+            ['r', 'Side_Right_Sensor', 'side_right_sensor', ],
+            ['r', 'Front_Right_Sensor', 'front_right_sensor', ],
+            ['r', 'Bottom_Right_Sensor', 'bottom_right_sensor', ],
+            ['r', 'Bottom_Left_Sensor', 'bottom_left_sensor', ],
+            ['r', 'Front_Left_Sensor', 'front_left_sensor', ],
+            ['r', 'Side_Left_Sensor', 'side_left_sensor', ]
         ]
     };
 
     // Register the extension
-    ScratchExtensions.register('My first extension', descriptor, ext);
+    ScratchExtensions.register('PHIRO', descriptor, ext);
 })({});
